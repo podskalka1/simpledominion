6a4f1c42502fbe4e904a316fcc23c405d98c890d
Spirintus <60117025+Spirintus@users.noreply.github.com>
2021-10-30 16:59:45 +0200

    Create Play.java

---
+++ src/main/java/sk/uniba/fmph/dcs/Play.java
@@ -0,0 +1,19 @@
+package sk.uniba.fmph.dcs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Play {
+    public ArrayList<CardInterface> cards = new ArrayList<>();
+
+    public void putTo(CardInterface card){
+        cards.add(card);
+    }
+
+    public List<CardInterface> throwAll(){
+        List<CardInterface> cardsToSend = cards;
+        cards = new ArrayList<>();
+        return cardsToSend;
+    }
+
+}
