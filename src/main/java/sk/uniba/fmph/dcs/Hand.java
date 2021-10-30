91d304b9218035a79fe399e98282a8c34f0f9f50
Spirintus <60117025+Spirintus@users.noreply.github.com>
2021-10-30 16:50:40 +0200

    Update Hand.java
    added constructor and method for getting cards during turn

--- src/main/java/sk/uniba/fmph/dcs/Hand.java
+++ src/main/java/sk/uniba/fmph/dcs/Hand.java
@@ -5,13 +5,24 @@ import java.util.ArrayList;
 public class Hand {
     public ArrayList<CardInterface> cards;
 
+    Deck deck;
+
+    public Hand(Deck deck){
+        this.deck = deck;
+        cards = new ArrayList<>(deck.draw(5));
+    }
+
     public Boolean isActionCard(int idx){
         if(idx>0 && idx< cards.size()) return cards.get(idx).cardType().isAction();
-        else return null; 
+        else return null;
     }
 
     public CardInterface play(int idx){
         if(idx>0 && idx< cards.size()) return cards.get(idx);
         else return null;
+    }
+
+    public void draw(int count){
+        cards.addAll(deck.draw(count));
     }
 }
