6017bf6dc07df2a03504453b3501cca8f8ac84a5
podskalka1 <podskalka1@uniba.sk>
2021-11-01 17:32:44 +0100

    Update Turn.java
    turned 7 buydecks into array

--- src/main/java/sk/uniba/fmph/dcs/Turn.java
+++ src/main/java/sk/uniba/fmph/dcs/Turn.java
@@ -1,19 +1,16 @@
 package sk.uniba.fmph.dcs;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Turn {
     TurnStatus turnStatus;
     DiscardPile discardPile;
     Deck deck;
     Hand hand;
-    BuyDeck market;
-    BuyDeck estate;
-    BuyDeck copper;
-    BuyDeck smithy;
-    BuyDeck village;
-    BuyDeck festival;
-    BuyDeck laboratory;
+    Play play;
+    ArrayList<BuyDeck> buyDecks;
+
 
     public Turn(TurnStatus turnStatus){
         this.turnStatus = turnStatus;
@@ -28,14 +25,16 @@ public class Turn {
 
         deck = new Deck(discardPile);
         hand = new Hand(deck);
+        buyDecks = new ArrayList<>(Arrays.asList(
+            new BuyDeck(10,GameCardType.GAME_CARD_TYPE_MARKET),
+            new BuyDeck(12,GameCardType.GAME_CARD_TYPE_ESTATE),
+            new BuyDeck(16,GameCardType.GAME_CARD_TYPE_COPPER),
+            new BuyDeck(10,GameCardType.GAME_CARD_TYPE_SMITHY),
+            new BuyDeck(10,GameCardType.GAME_CARD_TYPE_VILLAGE),
+            new BuyDeck(10,GameCardType.GAME_CARD_TYPE_FESTIVAL),
+            new BuyDeck(10,GameCardType.GAME_CARD_TYPE_LABORATORY)
+        ));
 
-        market = new BuyDeck(10,GameCardType.GAME_CARD_TYPE_MARKET);
-        estate = new BuyDeck(12,GameCardType.GAME_CARD_TYPE_ESTATE);
-        copper = new BuyDeck(16,GameCardType.GAME_CARD_TYPE_COPPER);
-        smithy = new BuyDeck(10,GameCardType.GAME_CARD_TYPE_SMITHY);
-        village = new BuyDeck(10,GameCardType.GAME_CARD_TYPE_VILLAGE);
-        festival = new BuyDeck(10,GameCardType.GAME_CARD_TYPE_FESTIVAL);
-        laboratory = new BuyDeck(10,GameCardType.GAME_CARD_TYPE_LABORATORY);
 
 
     }
